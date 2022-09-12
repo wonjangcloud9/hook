eb966f70df5134237c0e637b14e17b0e3df566f1
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-09-12 11:19:58 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import { useState } from "react";
 import Info from "./Info";
 
 function App() {
-  const [visible, setVisible] = useState(false);
   return (
     <>
-      <button onClick={() => setVisible(!visible)}>
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
+      <Info />
     </>
   );
 }
--- src/Info.js
+++ src/Info.js
@@ -1,38 +1,33 @@
-import { useState, useEffect } from "react";
+import { useReducer } from "react";
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
-
-  useEffect(() => {
-    console.log("렌더링이 완료되었습니다.");
-    console.log(name);
-    return () => {
-      console.log("cleanup");
-      console.log(name);
+  function reducer(state, action) {
+    return {
+      ...state,
+      [action.name]: action.value,
     };
-  }, [name]);
+  }
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
   };
-
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
-
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
-          <b>Name: </b> {name}
+          <b>이름:</b> {name}
         </div>
         <div>
-          <b>Nickname: </b> {nickname}
+          <b>닉네임:</b> {nickname}
         </div>
       </div>
     </div>
